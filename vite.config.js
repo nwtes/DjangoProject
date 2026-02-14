e9e4e6837e6159725bd77e312834e95fb452319f
nwte <dansir1405@gmail.com>
2026-02-14 18:12:31 +0000

    Fix Vite base for production

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/static_build/",
-  publicDir: false,   // optional but recommended
-  server: {
-    host: true,
-    port: 5173,
-    cors: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  },
+  publicDir: false,
   build: {
     manifest: true,
     outDir: "static_build",
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        "static/js/editor.js": "static/js/editor.js"
+        "js/editor.js": "static/js/editor.js"
       }
     }
   }
